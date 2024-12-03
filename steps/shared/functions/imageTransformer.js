fe4e9e0ea730511210227b0c58562256cf31532e
Antoine RICHARD <antoine.richard@partner.kiabi.com>
2024-12-03 18:20:07 +0100

    fix: image optimizer

--- steps/shared/functions/imageTransformer.js
+++ steps/shared/functions/imageTransformer.js
@@ -1,10 +1,16 @@
+import { fileURLToPath } from 'url';
+
 import path from 'path';
 import sharp from 'sharp';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const imageTransformerHook = async (request, reply) => {
   if (request.url.startsWith('/images')) {
     const transformations = {};
     if (request.query.width) transformations.width = Number(request.query.width);
+    if (request.query.height) transformations.height = Number(request.query.height);
     if (request.query.quality) transformations.quality = Number(request.query.quality);
     if (['webp', 'png', 'jpg', 'avif'].includes(request.query.format)) transformations.format = request.query.format;
 
@@ -17,7 +23,7 @@ export const imageTransformerHook = async (request, reply) => {
       image = image.toFormat(format, { quality: transformations.quality || 100, force: false });
 
       if (transformations.width) {
-        image = image.resize(transformations.width);
+        image = image.resize(transformations.width, transformations.height);
       }
 
       const bufferImage = await image.toBuffer();
