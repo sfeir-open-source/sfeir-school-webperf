a57e9ba2837069e0b84ded5226e0223a55b5b364
Antoine Richard <56544866+arichard-info@users.noreply.github.com>
2024-12-04 01:06:33 +0100

    feat(labs): 03.01 (#38)
    * feat(labs): 03.01
    
    * sync scripts
    
    * feat: re-sync all labs
    
    * feat: init priority lab
    
    ---------
    
    Co-authored-by: Antoine RICHARD <antoine.richard@partner.kiabi.com>

--- steps/03.03-cache-demo/public/scripts/tracking-1234.js
+++
@@ -1,6 +0,0 @@
-window.sendTrackingEvent = (eventName) => {
-  const start = performance.now();
-  // Simulate some heavy operations
-  while (performance.now() - start < 1000) {}
-  console.log('TRACK EVENT', eventName);
-};
